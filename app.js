154ca27bb2a412065b53e09cfe5e3145d711995b
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-01-28 12:10:50 +0000

    Update titles

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const app = express()
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index", { title: "Home" })
 })
 
 app.get("/about", (req, res) => {
-    res.render("about")
+    res.render("about", { title: "About" })
 })
 
 app.get("/about-me", (req, res) => {
@@ -19,11 +19,11 @@ app.get("/about-me", (req, res) => {
 })
 
 app.get("/blogs/create", (req, res) => {
-    res.render("create")
+    res.render("create", { title: "Create a new Blog" })
 })
 
 app.use((req, res) => {
-    res.status(404).render("404")
+    res.status(404).render("404", { title: "404" })
 })
 
 app.listen("3000", () => console.log("Server running..."))
