ae1d09202a4dddb16a2c6111d04577fa603bc3d3
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-01-28 11:44:02 +0000

    Create express app

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const fs = require("fs")
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.sendFile("./public/index.html", { root: __dirname })
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile("./public/about.html", { root: __dirname })
+})
+
+app.get("/about-me", (req, res) => {
+    res.redirect("/about")
+})
+
+app.use((req, res) => {
+    res.sendFile("./public/404.html", { root: __dirname })
+})
+
+app.listen("3000", () => console.log("Server running..."))
